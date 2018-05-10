c4cffc821f3b760ec05d031dbe47d73e32addc91
Diego Rojas <diego@brasa.art.br>
2018-05-10 16:30:26 -0300

    mais ajustes na single premios, atualizacao de email para usuário

--- inc/email-user.php
+++ inc/email-user.php
@@ -1,11 +1,11 @@
 <html>
-<p>Agradecemos sua inscrição no Prêmio EDP nas Artes!</p>
+<p>Agradecemos sua inscrição no Prêmio de Design Instituto Tomie Ohtake Leroy Merlin!</p>
 
 <p>Até o dia 08 de junho de 2018 será possível realizar a edição de seu cadastro, bem como excluir os arquivos já enviados e substituí-los por novos arquivos. Basta salvar as alterações realizadas no sistema, clicando em “atualizar”.</p>
 
-<p>Dúvidas podem ser enviadas ao email: premioedpnasartes@institutotomieohtake.org.br</p>
+<p>Dúvidas podem ser enviadas ao email: premiodesign@institutotomieohtake.org.br</p>
 
 <p>Boa sorte!</p>
 
-<p>Equipe do Prêmio EDP nas Artes 2018</p>
+<p>Equipe do Prêmio de Design Instituto Tomie Ohtake Leroy Merlin</p>
 </html>
